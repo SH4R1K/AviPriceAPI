3432007a3504ef1c1c89b2aeb15bcf71c63dfbae
Яков Хватов <YakovX95@mail.ru>
2024-03-09 22:24:03 +0300

    History

---
+++ AviPriceUI/Controllers/HistoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AviPriceUI.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult Index() // History
+        {
+            return View();
+        }
+    }
+}
