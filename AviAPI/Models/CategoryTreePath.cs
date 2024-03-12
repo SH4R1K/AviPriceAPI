4b61e1ef640474f022608268b9fc81f7d9758d60
SH4R1K <0504051@mail.ru>
2024-03-12 10:29:25 +0300

    db update

---
+++ AviAPI/Models/CategoryTreePath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AviAPI.Models;
+
+public partial class CategoryTreePath
+{
+    public int Ancestor { get; set; }
+
+    public int Descendant { get; set; }
+
+    public int Depth { get; set; }
+
+    public virtual Category AncestorNavigation { get; set; } = null!;
+
+    public virtual Category DescendantNavigation { get; set; } = null!;
+}
---
+++ AviAPI/Models/LocationTreePath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AviAPI.Models;
+
+public partial class LocationTreePath
+{
+    public int Ancestor { get; set; }
+
+    public int Descendant { get; set; }
+
+    public int Depth { get; set; }
+
+    public virtual Location AncestorNavigation { get; set; } = null!;
+
+    public virtual Location DescendantNavigation { get; set; } = null!;
+}
